92bb1fa3f2325f6a6d298673b9e9c1b4243164e3
tarifSe <gmtariful@gmail.com>
2021-03-05 01:02:28 +0600

    CategoryController&View_Added

---
+++ ROM/ROM/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ROM.DBContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ROM.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ROMDatabaseContext _context;
+        public CategoryController(ROMDatabaseContext rOMDatabaseContext)
+        {
+            _context = rOMDatabaseContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Save()
+        {
+            return View();
+        }
+    }
+}
